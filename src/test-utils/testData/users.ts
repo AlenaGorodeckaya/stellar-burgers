666cb81c7f3809abc5792616f1c6a2a7c50d4ebc
Alena Gorodeckaya <al.gorodeckaya@yandex.ru>
2025-09-20 21:07:04 +0700

    тесты на Cypress

---
+++ src/test-utils/testData/users.ts
@@ -0,0 +1,11 @@
+// Массив пользователей
+
+export const userTest = {
+  email: 'test-user@burger.com',
+  name: 'Алёна'
+};
+
+export const userBasic = {
+  email: 'basic@burger.com',
+  name: 'Обычный пользователь Ирина'
+};
